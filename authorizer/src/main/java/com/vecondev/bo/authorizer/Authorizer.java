dfa4a3f100dc5b5f306f9be281d021c214f92b49
Vache Khachatryan vache.khachatryan1998@gmail.com <vache.khachatryan1998@gmail.com>
2022-09-16 10:46:51 +0400

    Refactoring lambda authorizer

--- authorizer/src/main/java/com/vecondev/bo/authorizer/Authorizer.java
+++ authorizer/src/main/java/com/vecondev/bo/authorizer/Authorizer.java
@@ -1,5 +1,9 @@
 package com.vecondev.bo.authorizer;
 
+import static com.vecondev.bo.authorizer.Error.ACCESS_TOKEN_EXPIRED;
+import static com.vecondev.bo.authorizer.Error.ACCESS_TOKEN_MISSING;
+import static com.vecondev.bo.authorizer.Error.INVALID_ACCESS_TOKEN;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -14,6 +18,7 @@ import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import java.security.interfaces.RSAPublicKey;
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,11 +29,17 @@ public class Authorizer
   public static final String USER_ID = "id";
   public static final String USER_AUTHORITIES = "authorities";
   public static final String USER_USERNAME = "username";
+
   public static final String EXECUTION_POLICY = "execute-api";
   public static final String ALLOW_INVOCATION = ":Invoke";
   public static final String VERSION = "2012-10-17";
   public static final String AUTHENTICATION_SUCCESS_RESULT = "Allow";
   public static final String AUTHENTICATION_FAIL_RESULT = "Deny";
+
+  public static final String ERROR_RESPONSE_STATUS_KEY = "status";
+  public static final String ERROR_RESPONSE_CODE_KEY = "errorCode";
+  public static final String ERROR_RESPONSE_TIMESTAMP_KEY = "timestamp";
+  public static final String ERROR_RESPONSE_MESSAGE_KEY = "message";
 
   public IamPolicyResponse handleRequest(APIGatewayCustomAuthorizerEvent event, Context context) {
     final LambdaLogger logger = context.getLogger();
@@ -37,30 +48,41 @@ public class Authorizer
     String auth = AUTHENTICATION_FAIL_RESULT;
     if (token != null) {
       token = token.replace(JwtConfigProperties.getAuthorizationHeaderPrefix(), "");
+      try {
+        JWTVerifier jwtVerifier =
+            JWT.require(Algorithm.RSA256((RSAPublicKey) CertificateManager.publicKey())).build();
+        DecodedJWT decodedJwt = jwtVerifier.verify(token);
+        params.put(USER_ID, decodedJwt.getClaim(USER_ID).asString());
+        params.put(USER_USERNAME, decodedJwt.getSubject());
+        params.put(USER_AUTHORITIES, decodedJwt.getClaim(USER_AUTHORITIES).toString());
+        auth = AUTHENTICATION_SUCCESS_RESULT;
+        logger.log("Successful authorization.");
+      } catch (TokenExpiredException ex) {
+        logger.log("The access token is expired.");
+        params.putAll(constructErrorResponse(ACCESS_TOKEN_EXPIRED));
+      } catch (JWTVerificationException ex) {
+        logger.log("The access token is invalid.");
+        params.putAll(constructErrorResponse(INVALID_ACCESS_TOKEN));
+      }
     } else {
       logger.log("The access token is missing.");
-      return null;
-    }
-
-    try {
-      JWTVerifier jwtVerifier =
-          JWT.require(Algorithm.RSA256((RSAPublicKey) CertificateManager.publicKey())).build();
-      DecodedJWT decodedJwt = jwtVerifier.verify(token);
-      params.put(USER_ID, decodedJwt.getClaim(USER_ID).asString());
-      params.put(USER_USERNAME, decodedJwt.getSubject());
-      params.put(USER_AUTHORITIES, decodedJwt.getClaim(USER_AUTHORITIES).toString());
-      auth = AUTHENTICATION_SUCCESS_RESULT;
-      logger.log("Successful authorization.");
-    } catch (TokenExpiredException ex) {
-      logger.log("The access token is expired.");
-    } catch (JWTVerificationException ex) {
-      logger.log("The access token is invalid.");
+      params.putAll(constructErrorResponse(ACCESS_TOKEN_MISSING));
     }
 
     return getResponse(event, auth, params);
   }
 
-  public IamPolicyResponse getResponse(
+  private Map<String, Object> constructErrorResponse(Error error) {
+    Map<String, Object> params = new HashMap<>();
+    params.put(ERROR_RESPONSE_STATUS_KEY, error.getHttpStatus());
+    params.put(ERROR_RESPONSE_CODE_KEY, error.getCode());
+    params.put(ERROR_RESPONSE_TIMESTAMP_KEY, Instant.now().toString());
+    params.put(ERROR_RESPONSE_MESSAGE_KEY, error.getMessage());
+
+    return params;
+  }
+
+  private IamPolicyResponse getResponse(
       APIGatewayCustomAuthorizerEvent event, String auth, Map<String, Object> params) {
     Statement statement =
         Statement.builder()
---
+++ authorizer/src/main/java/com/vecondev/bo/authorizer/Error.java
@@ -0,0 +1,31 @@
+package com.vecondev.bo.authorizer;
+
+public enum Error {
+
+  ACCESS_TOKEN_MISSING(4012, Error.UNAUTHORIZED_HTTP_STATUS, "Access Token Missing"),
+  INVALID_ACCESS_TOKEN(4013, Error.UNAUTHORIZED_HTTP_STATUS, "Invalid Access Token"),
+  ACCESS_TOKEN_EXPIRED(4014, Error.UNAUTHORIZED_HTTP_STATUS, "Expired Access Token");
+
+  public static final String UNAUTHORIZED_HTTP_STATUS = "UNAUTHORIZED";
+  private final Integer code;
+  private final String httpStatus;
+  private final String message;
+
+  Error(Integer code, String httpStatus, String message) {
+    this.code = code;
+    this.httpStatus = httpStatus;
+    this.message = message;
+  }
+
+  public Integer getCode() {
+    return code;
+  }
+
+  public String getHttpStatus() {
+    return httpStatus;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+}
